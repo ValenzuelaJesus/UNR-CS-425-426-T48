dbe25b9b460178f46b25853df01bf91ad23e0afb
Leonel Covarrubias <77948434+Leonel-covarrubias@users.noreply.github.com>
2023-11-28 19:19:27 -0800

    Adding template of MVC for the AR environment

---
+++ ARView.java
@@ -0,0 +1,53 @@
+// View: Responsible for displaying data to the user
+public class ARView {
+  // Add necessary fields for UI elements and context
+
+  // Method to display data on the screen
+  public void displayData(String data) {
+    // Implement UI update logic here
+  }
+
+  // Method to clear the displayed data on the screen
+  public void clearData() {
+    // Implement UI clearing logic here
+  }
+}
+
+// Controller: Manages user interactions and coordinates the Model and View
+public class ARController {
+  private ARModel arModel;
+  private ARView arView;
+
+  // Constructor to initialize the Model and View
+  public ARController(ARModel arModel, ARView arView) {
+    this.arModel = arModel;
+    this.arView = arView;
+  }
+
+  // Method to handle user pointing the camera at a building
+  public void onBuildingIdentified(String buildingIdentifier) {
+    // Perform actions when a building is identified
+    // - Fetch relevant data
+    arModel.fetchDataForBuilding(buildingIdentifier);
+
+    // - Update the View to display the data
+    arView.displayData(/* Pass relevant data from the Model */);
+  }
+
+  // Method to handle user selecting to go back to the main menu
+  public void onBackToMainMenuSelected() {
+    // Implement navigation logic to go back to the main menu
+  }
+
+  // Method to handle user selecting to clear the data on the screen
+  public void onClearDataSelected() {
+    // Clear data in the Model
+    // Update the View to clear the displayed data
+    arView.clearData();
+  }
+
+  // Method to handle user selecting to go to an alternate page
+  public void onGoToAlternatePageSelected() {
+    // Implement navigation logic to go to the alternate page
+  }
+}
