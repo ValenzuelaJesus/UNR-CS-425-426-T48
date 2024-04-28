5189042dafb371f8c3f126ee9937fba3a866826f
m8ttt <58528155+m8ttt@users.noreply.github.com>
2024-04-28 01:43:32 -0700

    Added to More Info page: Updated Resources, Restrooms, Elevators, Staircase, & Amenities sections

--- app/src/main/java/com/example/senior/MoreInfo.java
+++ app/src/main/java/com/example/senior/MoreInfo.java
@@ -1,22 +1,24 @@
 package com.example.senior;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import com.example.senior.databinding.ActivityMoreInfoBinding;
 import com.google.common.util.concurrent.ListenableFuture;
 
@@ -68,11 +70,13 @@ public class MoreInfo extends AppCompatActivity {
     private TextView buildingCode;
     private TextView buildingNumber;
     private TextView hours;
-    private TextView resources;
+    private TextView resourcesTextView;
+    private ListView resourcesListView;
     private TextView restrooms;
     private TextView elevators;
     private TextView staircases;
-    private TextView amenities;
+    private TextView amenitiesTextView;
+    private ListView amenitiesListView;
     private TextView diningOptionsTextView;
     private ListView diningOptionsListView;
     private int buildingId;
@@ -119,11 +123,13 @@ public class MoreInfo extends AppCompatActivity {
         buildingCode = findViewById(R.id.building_code_textView);
         buildingNumber = (findViewById(R.id.building_number_textView));
         hours = (findViewById(R.id.hours_textView));
-        resources = (findViewById(R.id.resources_textView));
+        resourcesListView = (findViewById(R.id.resources_listView));
+        resourcesTextView = (findViewById(R.id.resources_textView));
         restrooms = (findViewById(R.id.restrooms_textView));
         elevators = (findViewById(R.id.elevators_textView));
         staircases = (findViewById(R.id.staircases_textView));
-        amenities = (findViewById(R.id.amenities_textView));
+        amenitiesListView = findViewById(R.id.amenities_listView);
+        amenitiesTextView = (findViewById(R.id.amenities_textView));
         diningOptionsListView = findViewById(R.id.dining_options_listView);
         diningOptionsTextView = findViewById(R.id.dining_options_textView);
 
@@ -162,6 +168,7 @@ public class MoreInfo extends AppCompatActivity {
         return null; // Return null if no building is found
     }
 
+    @SuppressLint("SetTextI18n")
     private void updateMoreInfo(Building building) {
         if (building != null) {
             buildingName.setText(building.getName());
@@ -169,9 +176,58 @@ public class MoreInfo extends AppCompatActivity {
             buildingNumber.setText(building.getBuildingNum());
             hours.setText(building.getOperatingHours());
 
-            List<DiningOption> diningOptions = findDiningOptionsByBuildingId(buildingId);
-            if (!diningOptions.isEmpty()) {
-                DiningOptionsAdapter adapter = new DiningOptionsAdapter(this, diningOptions);
+            List<String> resourcesList = buildResourcesList(building);
+            if (!resourcesList.isEmpty()) {
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, resourcesList);
+                resourcesListView.setAdapter(adapter);
+                resourcesListView.setVisibility(View.VISIBLE);
+                resourcesTextView.setVisibility(View.GONE);
+                updateRestroomTitleConstraints();
+            } else {
+                resourcesTextView.setText("N/A");
+                resourcesTextView.setVisibility(View.VISIBLE);
+                resourcesListView.setVisibility(View.GONE);
+                updateRestroomTitleConstraints();
+            }
+
+            Restroom restroom = findRestroomByBuildingId(buildingId);
+            if (restroom!= null) {
+                restrooms.setText(restroom.getLocation());
+            } else {
+                restrooms.setText("N/A");
+            }
+
+            Elevator elevator = findElevatorByBuildingId(buildingId);
+            if (elevator != null) {
+                elevators.setText(elevator.getLocation());
+            } else {
+                elevators.setText("N/A");
+            }
+
+            Staircase staircase = findStaircaseByBuildingId(buildingId);
+            if (staircase!= null) {
+                staircases.setText(staircase.getLocation());
+            } else {
+                staircases.setText("N/A");
+            }
+
+            List<String> amenitiesList = buildAmenitiesList(building);
+            if (!amenitiesList.isEmpty()) {
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, amenitiesList);
+                amenitiesListView.setAdapter(adapter);
+                amenitiesListView.setVisibility(View.VISIBLE);
+                amenitiesTextView.setVisibility(View.GONE);
+                updateDiningOptionsTitleConstraints();
+            } else {
+                amenitiesTextView.setText("N/A");
+                amenitiesTextView.setVisibility(View.VISIBLE);
+                amenitiesListView.setVisibility(View.GONE);
+                updateDiningOptionsTitleConstraints();
+            }
+
+            List<String> diningOptionsList = buildDiningOptionsList(building);
+            if (!diningOptionsList.isEmpty()) {
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, diningOptionsList);
                 diningOptionsListView.setAdapter(adapter);
                 diningOptionsListView.setVisibility(View.VISIBLE);
                 diningOptionsTextView.setVisibility(View.GONE);
@@ -181,12 +237,58 @@ public class MoreInfo extends AppCompatActivity {
                 diningOptionsListView.setVisibility(View.GONE);
             }
         } else {
-            buildingName.setText("Building not found");
-            buildingCode.setText("");
-            buildingNumber.setText("");
-            hours.setText("");
-            diningOptionsTextView.setText("");
+            buildingName.setText("Building Not Found");
+            buildingCode.setText("N/A");
+            buildingNumber.setText("N/A");
+            hours.setText("N/A");
+            resourcesTextView.setText("N/A");
+            restrooms.setText("N/A");
+            elevators.setText("N/A");
+            staircases.setText("N/A");
+            amenitiesTextView.setText("N/A");
+            diningOptionsTextView.setText("N/A");
         }
+    }
+
+
+    private Restroom findRestroomByBuildingId(int buildingId) {
+        for (Restroom restroom : AllRestrooms) {
+            if (restroom.getBuilding() == buildingId) {
+                return restroom;
+            }
+        }
+        return null;
+    }
+    private Elevator findElevatorByBuildingId(int buildingId) {
+        for (Elevator elevator : AllElevators) {
+            if (elevator.getBuilding() == buildingId) {
+                return elevator;
+            }
+        }
+        return null;
+    }
+
+    private Staircase findStaircaseByBuildingId(int buildingId) {
+        for (Staircase staircase : AllStaircases) {
+            if (staircase.getBuilding() == buildingId) {
+                return staircase;
+            }
+        }
+        return null;
+    }
+
+    private List<String> buildDiningOptionsList(Building building) {
+        List<String> diningOptionsList = new ArrayList<>();
+        List<DiningOption> diningOptions = findDiningOptionsByBuildingId(building.getId());
+        for (DiningOption diningOption : diningOptions) {
+            diningOptionsList.add(diningOption.getDescription());
+            diningOptionsList.add(diningOption.getLocation());
+            diningOptionsList.add(diningOption.getOperatingHours());
+            diningOptionsList.add(diningOption.getWeblink());
+            diningOptionsList.add(diningOption.getMenulink());
+            diningOptionsList.add("");
+        }
+        return diningOptionsList;
     }
 
     private List<DiningOption> findDiningOptionsByBuildingId(int buildingId) {
@@ -198,37 +300,112 @@ public class MoreInfo extends AppCompatActivity {
         }
         return diningOptions;
     }
+    private List<String> buildResourcesList(Building building) {
+        List<String> resourcesList = new ArrayList<>();
 
-    private String getDiningOptionsString(DiningOption diningOption) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(diningOption.getDescription()).append("\n");
-        sb.append(diningOption.getLocation()).append("\n");
-        sb.append(diningOption.getOperatingHours()).append("\n");
-        sb.append(diningOption.getWeblink()).append("\n");
-        sb.append(diningOption.getMenulink()).append("\n\n");
-        return sb.toString();
+        for (Resource resource : AllResources) {
+            if (resource.getBuilding() == building.getId()) {
+                resourcesList.add(resource.getDescription());
+                resourcesList.add(resource.getLocation());
+                resourcesList.add(resource.getOperatingHours());
+                resourcesList.add(resource.getWeblink());
+                resourcesList.add("");
+            }
+        }
+
+        for (Lab lab : AllLabs) {
+            if (lab.getBuilding() == building.getId()) {
+                resourcesList.add("Lab: " + lab.getType());
+                resourcesList.add(lab.getLocation());
+                resourcesList.add("");
+            }
+        }
+
+        for (Library library : AllLibraries) {
+            if (library.getBuilding() == building.getId()) {
+                resourcesList.add("Library: " + library.getDescription());
+                resourcesList.add(library.getLocation());
+                resourcesList.add("");
+            }
+        }
+
+        return resourcesList;
     }
 
-    private class DiningOptionsAdapter extends ArrayAdapter<DiningOption> {
-        private List<DiningOption> diningOptions;
+    private void updateRestroomTitleConstraints() {
+        ConstraintLayout constraintLayout = findViewById(R.id.UI);
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(constraintLayout);
 
-        public DiningOptionsAdapter(Context context, List<DiningOption> diningOptions) {
-            super(context, android.R.layout.simple_list_item_1, diningOptions);
-            this.diningOptions = diningOptions;
+        if (resourcesListView.getVisibility() == View.VISIBLE) {
+            constraintSet.connect(R.id.restrooms_title, ConstraintSet.TOP, R.id.resources_listView, ConstraintSet.BOTTOM, 8);
+        } else if (resourcesTextView.getVisibility() == View.VISIBLE) {
+            constraintSet.connect(R.id.restrooms_title, ConstraintSet.TOP, R.id.resources_textView, ConstraintSet.BOTTOM, 8);
         }
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view = super.getView(position, convertView, parent);
-            TextView textView = (TextView) view.findViewById(android.R.id.text1);
-            textView.setText(getDiningOptionsString(diningOptions.get(position)));
-            return view;
+        constraintSet.applyTo(constraintLayout);
+    }
+
+    private List<String> buildAmenitiesList(Building building) {
+        List<String> amenitiesList = new ArrayList<>();
+
+        for (Hangoutspots hangoutspots : AllHangoutspots) {
+            if (hangoutspots.getBuilding() == building.getId()) {
+                amenitiesList.add("Hangout Spots: " + hangoutspots.getDescription());
+                amenitiesList.add(hangoutspots.getLocation());
+                amenitiesList.add("");
+            }
         }
+
+        for (SpecialFeature specialFeature : AllSpecialFeatures) {
+            if (specialFeature.getBuilding() == building.getId()) {
+                amenitiesList.add("Special Features: " + specialFeature.getDescription());
+                amenitiesList.add(specialFeature.getLocation());
+                amenitiesList.add("");
+            }
+        }
+
+        for (Store store : AllStores) {
+            if (store.getBuilding() == building.getId()) {
+                amenitiesList.add("Stores: " + store.getDescription());
+                amenitiesList.add(store.getLocation());
+                amenitiesList.add("");
+            }
+        }
+
+        for (VendingMachine vendingMachine : AllVendingMachines) {
+            if (vendingMachine.getBuilding() == building.getId()) {
+                amenitiesList.add("Vending Machines: " + vendingMachine.getType());
+                amenitiesList.add(vendingMachine.getLocation());
+                amenitiesList.add("");
+            }
+        }
+
+        return amenitiesList;
+    }
+
+    private void updateDiningOptionsTitleConstraints() {
+        ConstraintLayout constraintLayout = findViewById(R.id.UI);
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(constraintLayout);
+
+        if (amenitiesListView.getVisibility() == View.VISIBLE) {
+            constraintSet.connect(R.id.dining_options_title, ConstraintSet.TOP, R.id.amenities_listView, ConstraintSet.BOTTOM, 8);
+        } else if (amenitiesTextView.getVisibility() == View.VISIBLE) {
+            constraintSet.connect(R.id.dining_options_title, ConstraintSet.TOP, R.id.amenities_textView, ConstraintSet.BOTTOM, 8);
+        }
+
+        constraintSet.applyTo(constraintLayout);
     }
 
     private void applyColorBlindMode(int colorBlindnessMode) {
         // Apply color blindness filter
         ColorBlind.applyColorBlindMode(getWindow().getDecorView().getRootView(), colorBlindnessMode);
+    }
+    private void openWeblink(String weblink) {
+        Uri uri = Uri.parse(weblink);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 
     public String connectToWebService(String urlString) {
